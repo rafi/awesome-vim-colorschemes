eb661a946ff82067b377bc7b60ec126a56ec15ad
Daksh Sharma <dashatrix@gmail.com>
2019-05-14 18:40:54 -0700

    pull update file updated to include tomorrow theme

--- pull-updates.sh
+++ pull-updates.sh
@@ -65,6 +65,7 @@ function _main() {
 	_dl scheakur/vim-scheakur
 	_dl sts10/vim-pink-moon
 	_dl tomasr/molokai
+	_dl chriskempson/tomorrow-theme/vim/colors
 	_dl tyrannicaltoucan/vim-deep-space
 	_dl vim-scripts/pyte
 	_dl vim-scripts/rdark-terminal2.vim
